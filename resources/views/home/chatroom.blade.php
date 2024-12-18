28ef56d3a9902c756ba92657cd0a7c43ac74889f
testing-xx <axelkurn@gmail.com>
2024-12-18 22:12:27 +0700

    Add: internal styling

--- resources/views/home/chatroom.blade.php
+++ resources/views/home/chatroom.blade.php
@@ -74,4 +74,119 @@
     })
     document.getElementById('')
 </script>
+<style>
+    .logo-container{
+    object-fit: cover;
+    height: 70px;
+    width: 50px;
+    border: 1px solid red;
+}
+
+::placeholder{
+    color: white;
+    font-size: 24px;
+    font-weight: 300;
+}
+
+.menu-button{
+    margin-left: 10px;
+    display: flex;
+    flex-direction: column;
+    row-gap: 8px;
+    cursor: pointer;
+}
+
+#sidebar{
+    background-color: #6C4F4F;
+}
+
+.rectangle{
+    height: 4px;
+    border: 3px solid #6C4F4F;
+    width: 40px;
+    border-radius: 2px;
+}
+
+.title{
+    color: #6C4F4F;
+    font-weight: 400;
+}
+
+.user-chat, .bot-chat {
+    background-color: #6C4F4F;
+    width: 45%;
+    color: white;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1)
+}
+
+.user-chat{
+    border-radius: 15px 0 15px 15px;
+}
+
+.bot-chat{
+    border-radius: 15px 15px 15px 0;
+}
+
+.prompt{
+    background-color: #6C4F4F;
+    border-radius: 5px;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+
+.prompt-button{
+    cursor: pointer;
+    height: 80%;
+    width: auto;
+    object-fit: cover;
+}
+
+input[type="text"] {
+    color: white;
+    background-color: white;
+}
+
+input[type="text"]::placeholder {
+    font-size: 1.1rem;
+    color: #aaa;
+}
+
+
+input:focus{
+    outline: none;
+}
+
+.logout-btn {
+    background: none;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+}
+
+.logout-btn img {
+    margin-right: 10px;
+    width: 50px;
+    height: auto;
+    transition: filter 0.3s ease-in-out;
+}
+
+@media screen and (max-width: 768px) {
+
+    .title{
+        display: none;
+    }
+
+    ::placeholder{
+        opacity: 0;
+        display: none;
+    }
+}
+
+::-webkit-scrollbar{
+    display: none;
+}
+</style>
 </html>
